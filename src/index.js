ab8d000a69d381d90180f74bb0957e8c42228191
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-25 16:15:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+function fetchBooks() {
+  return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+}
+
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks()
+})
